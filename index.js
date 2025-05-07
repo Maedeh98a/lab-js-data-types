5a6ce35cb67603e4aa12d7677b88214a7796a00c
Maedeh98a <maedeh.ahmadian98@gmail.com>
2025-05-07 14:38:21 +0200

    Solved lab

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const s4 = "bread";
 const s5 = "and";
 
 // Concatenate the string variables into one new string
+let tongueTwister = `${s1} ${s2} ${s3} ${s4} ${s5} ${s3} ${s2} ${s1} ${s4}.`;
+console.log(tongueTwister)
 
 
 // Print out the concatenated string
@@ -22,12 +24,12 @@ const part1 = "java";
 const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
+let new_part1 = part1.slice(0, part1.length - 1) + part1[part1.length - 1].toUpperCase()
+let new_part2 = part2.slice(0, part2.length - 1) + part2[part2.length - 1].toUpperCase()
 
-
+let result = new_part1 + new_part2
 // Print the cameLtaiL-formatted string
-
-
-
+console.log(result)
 
 /*******************************************
     Iteration 2.1 | Calculate Tip
@@ -35,7 +37,9 @@ const part2 = "script";
 const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
+let tipAmount = billTotal * 0.15
 
+console.log(tipAmount)
 
 // Print out the tipAmount
 
@@ -48,10 +52,10 @@ const billTotal = 84;
 
 // Generate a random integer between 1 and 10 (inclusive)
 
-
+let randomNumber = Math.random(1, 10) * 10;
 // Print the generated random number
 
-
+console.log(Math.round(randomNumber))
 
 /*******************************************
     Iteration 3.1 | Booleans
@@ -62,15 +66,29 @@ const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
 const expression1 = a && b;
+console.log("My answer: False");
+console.log(expression1);
 
 const expression2 = a || b;
+console.log("My answer: True");
+console.log(expression2);
 
 const expression3 = !a && b;
+console.log("My answer: False");
+console.log(expression3);
 
 const expression4 = !(a && b);
+console.log("My answer: True");
+console.log(expression4);
 
 const expression5 = !a || !b;
+console.log("My answer: True");
+console.log(expression5);
 
 const expression6 = !(a || b);
+console.log("My answer: False");
+console.log(expression6);
 
 const expression7 = a && a;
+console.log("My answer: True");
+console.log(expression7);
